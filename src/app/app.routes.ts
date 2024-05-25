2bc103bbe77c15b2e0c2da4734724006765c5d00
=JeanJacques <jjn99@outlook.fr>
2024-05-25 21:42:31 +0000

    feat: prepare routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,48 @@
 import { Routes } from '@angular/router';
+import {AppLayoutComponent} from "./layout/app.layout.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'auth',
+    pathMatch: 'full'
+  },
+  {
+    path: 'auth',
+    data: { breadcrumb: 'Auth' }
+  },
+  {
+    path: '',
+    component: AppLayoutComponent,
+    children: [
+      {
+        path: 'home'
+      },
+      {
+        path: 'personnel'
+      },
+      {
+        path: 'conge-absence'
+      },
+      {
+        path: 'social-assurance'
+      },
+      {
+        path: 'fonction-list'
+      },
+      {
+        path: 'couriers'
+      },
+      {
+        path: 'remunerations'
+      }
+    ]
+  },
+  {
+    path: 'notfound'
+  },
+  {
+    path: '**',
+    redirectTo: '/notfound'
+  }
+];

172110b13f030bf073e513e98b434e63b6c7bb8d
=JeanJacques <jjn99@outlook.fr>
2024-05-27 15:58:33 +0000

    feat: add crud operation in personnel service

--- src/app/core/data/personals/personnel.service.ts
+++ src/app/core/data/personals/personnel.service.ts
@@ -1,9 +1,45 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Dossier, PersonnelRequest, PersonnelResponse} from "./personnel.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonnelService {
 
-  constructor() { }
+  // url du server Json
+  url = 'http://localhost:3000/personnels';
+
+  // Inject HttpClient
+  http = inject(HttpClient);
+
+  // get all personnels
+  getAllPersonnels(){
+    return this.http.get<PersonnelResponse[]>(this.url);
+  }
+
+  // get personnel by id
+  getPersonnelById(id: string){
+    return this.http.get<PersonnelResponse>(`${this.url}/${id}`);
+  }
+
+  // create personnel
+  addPersonnel(personnel: PersonnelRequest){
+    return this.http.post(this.url, personnel);
+  }
+
+  // update personnel
+  updatePersonnel(id: string, personnel: PersonnelRequest){
+    return this.http.patch(`${this.url}/${id}`, personnel);
+  }
+
+  // delete personnel
+  deletePersonnel(personnel: PersonnelResponse){
+    return this.http.delete(`${this.url}/${personnel.id}`);
+  }
+
+  //   get all dossiers
+  getAllDossiers(){
+    return this.http.get<Dossier[]>(`${this.url}/dossiers`);
+  }
 }

d78476b57d6ebbfea54d27c0f3fc6ccb7039b3f2
=JeanJacques <jjn99@outlook.fr>
2024-06-23 23:26:20 +0000

    add file

---
+++ src/app/etats/etats-personnel/etat-disponibilite/etat-disponibilite.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mrt-etat-disponibilite',
+  standalone: true,
+  imports: [],
+  templateUrl: './etat-disponibilite.component.html',
+  styleUrl: './etat-disponibilite.component.scss'
+})
+export class EtatDisponibiliteComponent {
+
+}
